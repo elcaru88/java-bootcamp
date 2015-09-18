af999c2b1b181602c0d273e17773341b80cd0c86
Fer-Cab <Ferjcb@live.com>
2015-09-18 07:54:23 -0300

    ejercicios 1-2 Topic 3

---
+++ Topic 3/Exercise1/src/exercise1/MiTest.java
@@ -0,0 +1,52 @@
+package exercise1;
+
+public class MiTest {
+	public static void main(String[] args) {
+		
+		
+		Converter con = new Converter();
+	//	con.setNumber(120.00);
+//		con.getText();
+		//System.out.println(con.decimalToText(0));
+	/*	String hola ="hola";
+		System.out.println(hola.length());
+		System.out.println(hola.charAt(0));*/
+		
+	//	String Snumber = Double.toString(1.20);
+	//	System.out.println(Snumber);
+	//	System.out.println(Snumber.length());
+	//	int i = (int) 10.02;
+	//	System.out.println(i);
+	
+	//	System.out.println(con.compositeNumber(25));
+	//	System.out.println(con.decimalToText(95));
+	//	String st = Integer.toString(25);
+		//	System.out.println(st.charAt(0));
+	//	String stringNumber = Double.toString(10050.23);
+	//	System.out.println(stringNumber.charAt(0));
+	//	System.out.println(Integer.parseInt(stringNumber.charAt(0)+""));
+		//System.out.println(con.decimalToText((int)stringNumber.charAt(0)));
+	//System.out.println(con.decimalToText((int)stringNumber.charAt(0)));
+		//System.out.println(Integer.parseInt(stringNumber.charAt(0)+""+stringNumber.charAt(1)));
+		
+		//System.out.println(Integer.parseInt(stringNumber.charAt(0)+ ""+stringNumber.charAt(1)+ "") );
+		
+		
+		con.setNumber(25500.0);
+		System.out.println(con.getText());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
