d09d16197d2728637074ed7aa67635e9e1116149
Fer-Cab <Ferjcb@live.com>
2015-10-07 10:51:28 -0300

    arreglo log in

--- FinalProject/FinalProject/src/main/java/com/controllers/AccountController.java
+++ FinalProject/FinalProject/src/main/java/com/controllers/AccountController.java
@@ -1,7 +1,5 @@
 package com.controllers;
 
-import javax.servlet.http.HttpServletResponse;
-
 import com.exception.UserExist;
 import com.exception.UserNotFoundWrongPassException;
 import com.model.Account;
@@ -12,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,13 +20,11 @@ public class AccountController {
 	private AccountService accountService;
 
 	@RequestMapping(method = RequestMethod.POST)
-	public void createAccount(
-			@RequestParam(value = "userName", required = true) String userName,
-			@RequestParam(value = "password", required = true) String password) {
-		if(!accountService.existUserName(userName))
-		accountService.createAccount(new Account(userName,password));
+	public void createAccount(@RequestBody Account account ) {
+		if(!accountService.existUserName(account.userName))
+		accountService.createAccount(account);
 		else
-		new UserExist(userName);
+			throw new UserExist(account.userName);
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
@@ -47,14 +42,12 @@ public class AccountController {
 	
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public void login(
-			@RequestParam(value = "userName", required = true) String userName,
-			@RequestParam(value = "password", required = true) String password,
-			HttpServletResponse response) throws Exception{
-		if(accountService.correctPass(userName, password))
-			response.sendRedirect("/"+userName+"/shoppingCart");
+	public String  login(@RequestBody Account account) {
+		if(accountService.equals(account))
+			return "user granted access";
+					
 		else
-			new UserNotFoundWrongPassException(userName);
+			throw new UserNotFoundWrongPassException(account.userName);
 		
 	}
 	
